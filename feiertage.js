7571bde2fcde84bb92c0e0d9b3d68cbaf0269db6
langm <lang.marius@t-online.de>
2020-12-27 23:38:12 +0100

    fixed holidays added

--- feiertage.js
+++ feiertage.js
@@ -10,12 +10,57 @@ module.exports = function(RED) {
         var currentYear = currentDate.getFullYear(); // full year
         var currentWeekday = currentDate.getDay(); // weekday
 
+        var newYear = currentYear + "-1-1"; // day of New Year
+        var holyThreeKings = currentYear + "-1-6"; // day of Holy Three Kings
+        var valentinstag = currentYear + "-2-14"; // day of Valentinstag
+        var mariaHimmelfahrt = currentYear + "-8-15" // day of Maria Himmelfahrt
+        var tagDerDeutschenEinheit = currentYear + "-10-3"; // day of Tag der Deutschen Einheit
+        var halloween = currentYear + "-10-31"; // day of Halloween
+        var allerheiligen = currentYear + "-11-1"; // day of Allerheiligen
+        var stMartin = currentYear + "-11-11"; // day of St. Martin
+        var bussUndBettag = currentYear + "-11-17"; // day of Buß und Bettag
+        var nikolaus = currentYear + "-12-6"; // day of Nikolaus
         var advent1; // day of first Advent
         var advent2; // day of second Advent
         var advent3; // day of third Advent
         var advent4; // day of fourth Advent
+        var christmasEve = currentYear + "-12-24"; // day of Christmas Eve
+        var firstDayChristmas = currentYear + "-12-25"; // day of First day of Christmas
+        var secondDayChristmas = currentYear + "-12-26"; // day of Second day of Christmas
+        var newYearsEve = currentYear + "-12-31"; // day of New Year
+
+        var holiday = [
+            newYear,
+            holyThreeKings,
+            valentinstag,
+            mariaHimmelfahrt,
+            tagDerDeutschenEinheit,
+            halloween,
+            allerheiligen,
+            stMartin,
+            bussUndBettag,
+            nikolaus,
+            advent1,
+            advent2,
+            advent3,
+            advent4,
+            christmasEve,
+            firstDayChristmas,
+            secondDayChristmas,
+            newYearsEve
+        ]; // array contains all holiday
+
+        var isTodayHoliday;
 
         this.on('input', function(msg) {
+            if (msg.payload == "isTodayHoliday") {
+                // output boolean is today holiday
+                isTodayHoliday();
+                node.send(isTodayHoliday);
+            }
+            else if (msg.payload == "nextHoliday") {
+                // outputs next holiday date and name
+            }
             advent(24);
         });
 
@@ -27,13 +72,25 @@ module.exports = function(RED) {
 
             // check generated weekday is 0 and generated month is 12
             if (checkWeekday === 0 && checkMonth == 12) {
-                advent4 = day;
-                advent3 = day - 7;
-                advent2 = day - 14;
-                advent1 = day - 21;
+                advent4 = currentYear + "-" + day + "-" + day;
+                advent3 = currentYear + "-" + day + "-" + day - 7;
+                advent2 = currentYear + "-" + day + "-" + day - 14;
+                advent1 = currentYear + "-" + day + "-" + day - 21;
             }
             else {
                 advent(day - 1);
+            }
+        }
+
+        function isTodayHoliday() {
+            // check if today is holiday
+            for (let i = 0; i < holiday.length; i++) {
+                if (new Date(holiday[i]) == new Date()) {
+                    isTodayHoliday = true;
+                }
+                else {
+                    isTodayHoliday = false;
+                }
             }
         }
     }
